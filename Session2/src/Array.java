0f10c0908312ef3cf4785943662d9abb60519b69
Nadeem <nadim70.na@gmail.com>
2020-12-30 21:15:58 +0530

    session1 and session2

---
+++ Session2/src/Array.java
@@ -0,0 +1,17 @@
+
+public class Array {
+
+	public static void main(String[] args) {
+		int a[] = {10,20,30,40};
+		for(int num:a) {
+		System.out.print(" " +num);
+		}
+		int b[];
+		b=new int[] {10,20,30,40};
+		System.out.println(" " +b[0]);
+		int arr[] = new int[5];
+		arr[0] = 10;
+		System.out.println(arr[0]);
+	}
+	}
+
---
+++ Session2/src/premitive.java
@@ -0,0 +1,16 @@
+
+public class premitive {
+
+	public static void main(String[] args) {
+		byte age = (byte)256;
+		System.out.println(age);
+		float num = (float)125.55;
+		char ch = 'A';
+		char ch1 = 97;
+		char ch2 = '\u2506';
+		System.out.println(ch);
+		System.out.println(ch1);
+		System.out.println(ch2);
+	}
+
+}
